85b5f6647270199c36a175e872a125c20c5ba171
Key150920 <sanchezkeily0726@gmail.com>
2023-09-12 16:12:49 -0500

    inicio

---
+++ src/routes/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Inicio from '../pages/Inicio';
+import Login from '../pages/Login';
+
+function App() {
+  return (
+    <Router>
+        <Routes>
+          <Route exact path='/' element={<Inicio/>}/>
+            <Route exact path='/Login' element={<Login/>}/>
+       </Routes>
+    </Router>
+    
+  );
+}
+
+export default App;
