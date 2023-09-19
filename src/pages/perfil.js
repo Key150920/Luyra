8c49f2217dcc21ea723895297ef4e5f91350198b
Angiezuzu <angiezuniga.p@gmail.com>
2023-09-19 16:21:45 -0500

    19/09/2023

---
+++ src/pages/perfil.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import Footer from '../componentes/footer';
+import '../estilos/main.css';
+
+function Perfil() {
+    return (
+        <div className='Perfil'>
+            <section class="vh-100" style={{backgroundColor: "#f4f5f7"}}>
+                <div class="container py-5 h-100">
+                    <div class="row d-flex justify-content-center align-items-center h-100">
+                        <div class="col col-lg-6 mb-4 mb-lg-0">
+                            <div class="card mb-3" style={{borderRadius: ".5rem"}}>
+                                <div class="row g-0">
+                                    <div class="col-md-4 gradient-custom text-center text-white"
+                                        style={{borderTopRightRadius: ".5rem", borderBottomLeftRadius:  ".5rem"}}>
+                                        <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp"
+                                            alt="Avatar" class="img-fluid my-5" style={{width: "80px"}}/>
+                                        <h5>Angie Zuñiga</h5>
+                                        <p>Web Designer</p>
+                                        <i class="far fa-edit mb-5"></i>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <div class="card-body p-4">
+                                            <h6>Información</h6>
+                                            <hr class="mt-0 mb-4"/>
+
+                                                <div class="row pt-1">
+                                                    <div class="col-6 mb-3">
+                                                        <h6>Email</h6>
+                                                        <p class="text-muted">angiezu@gmail.com</p>
+                                                    </div>
+                                                    <div class="col-6 mb-3">
+                                                        <h6>Celular</h6>
+                                                        <p class="text-muted">64283663</p>
+                                                    </div>
+                                                </div>
+                                                <h6>Más información</h6>
+                                                <hr class="mt-0 mb-4"/>
+                                                    <div class="row pt-1">
+                                                        <div class="col-6 mb-3">
+                                                            <h6>Fecha de nacimiento</h6>
+                                                            <p class="text-muted">23/06/2002</p>
+                                                        </div>
+                                                        <div class="col-6 mb-3">
+                                                            <h6>Sexo</h6>
+                                                            <p class="text-muted">Mujer</p>
+                                                        </div>
+                                                    </div>
+                                                    <div class="d-flex justify-content-start">
+                                                        <a href="#!"><i class="fab fa-facebook-f fa-lg me-3"></i></a>
+                                                        <a href="#!"><i class="fab fa-twitter fa-lg me-3"></i></a>
+                                                        <a href="#!"><i class="fab fa-instagram fa-lg"></i></a>
+                                                    </div>
+                                                </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            </section>
+
+            <Footer />
+        </div>
+
+
+    );
+}
+
+export default Perfil;
--- src/routes/App.js
+++ src/routes/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Inicio from '../pages/Inicio';
 import Login from '../pages/Login';
+import Registro from '../pages/Registro';
+import Perfil from '../pages/perfil';
 
 function App() {
   return (
     <Router>
         <Routes>
           <Route exact path='/' element={<Inicio/>}/>
-            <Route exact path='/Login' element={<Login/>}/>
+          <Route exact path='/Login' element={<Login/>}/>
+          <Route exact path='/registro' element={<Registro/>}/>
+          <Route exact path='/perfil' element={<Perfil/>}/>
        </Routes>
     </Router>
     
